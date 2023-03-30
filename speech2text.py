dccfa0ec58fbd9c307c75a21c32d44f8f03550d5
AtsuneMogi <maomudunyin@gmail.com>
2023-03-30 12:30:06 +0900

    first commit

---
+++ speech2text.py
@@ -0,0 +1,61 @@
+import openai
+import serial
+import sounddevice as sd
+import sys
+import wavio as wv
+from scipy.io.wavfile import write
+
+
+ser = serial.Serial(
+    port="/dev/ttyACM0",
+    baudrate=115200
+)
+openai.api_key = "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"
+# Sampling frequency
+freq = 44100
+# Recording duration
+duration = 5
+
+
+def main():
+    ser.close()
+    ser.open()
+    while (1):
+        print("\n\nSay something:")
+        # Start recorder with the given values
+        # of duration and sample frequency
+        recording = sd.rec(
+            int(duration * freq),
+            samplerate=freq, channels=2
+        )
+        # Record audio for the given number of seconds
+        sd.wait()
+        # This will convert the NumPy array to an audio
+        # file with the given sampling frequency
+        write("recording0.wav", freq, recording)
+        # Convert the NumPy array to audio file
+        wv.write("recording1.wav", recording, freq, sampwidth=2)
+        file = open("recording1.wav", "rb")
+        text = openai.Audio.transcribe("whisper-1", file)["text"]
+        print(text)
+        if ("おしまい" in text):
+            sys.exit(1)
+        ser.write((text + "\n").encode())
+        print("Sent with success!")
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+# Note: you need to be using OpenAI Python v0.27.0 for the code below to work
+openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Who won the world series in 2020?"},
+        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+        {"role": "user", "content": "Where was it played?"}
+    ]
+)
